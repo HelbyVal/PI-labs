de6dba859281e0e855273e250f11fe186f85f9cf
Vaik Helby <valientin.rudin@mail.ru>
2025-11-05 20:51:42 +0500

    Еще мелкие исправления

--- 3lab-mini.py
+++ 3lab-mini.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from logic import mean_fare 
 
+
 df = pd.read_csv("titanic.csv", sep=",")
 
 box_options = {"Шебурн": "S", "Квинстаун": "Q", "Саутгемптон": "C"}
--- test_lab3.py
+++ test_lab3.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 import numpy as np
-import logic as l
+from logic import mean_fare
 
 
 @pytest.fixture
@@ -15,28 +15,27 @@ def titanic_df():
 
 
 def test_mean_fare_survived_S(titanic_df):
-    result = l.mean_fare(titanic_df, "S", True)
+    result = mean_fare(titanic_df, "S", True)
     assert result == 10
 
 
 def test_mean_fare_dead_S(titanic_df):
-    result = l.mean_fare(titanic_df, "S", False)
+    result = mean_fare(titanic_df, "S", False)
     assert result == 20
 
 
 def test_mean_fare_survived_C(titanic_df):
-    result = l.mean_fare(titanic_df, "C", True)
+    result = mean_fare(titanic_df, "C", True)
     assert result == 30
 
 
 def test_mean_fare_dead_Q(titanic_df):
-    result = l.mean_fare(titanic_df, "Q", False)
+    result = mean_fare(titanic_df, "Q", False)
     assert result == 60
 
 
 def test_mean_fare_nan(titanic_df):
-    s = l.mean_fare(titanic_df, "X", True)
-    d = l.mean_fare(titanic_df, "X", False)
+    s = mean_fare(titanic_df, "X", True)
+    d = mean_fare(titanic_df, "X", False)
     assert np.isnan(s)
     assert np.isnan(d)
-    
